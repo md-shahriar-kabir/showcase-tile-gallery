f96530843101685652f7287819cba6ce619dcb30
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-03 13:12:05 +0600

    Updated Loading file

--- src/component/LatestTiles.jsx
+++ src/component/LatestTiles.jsx
@@ -1,28 +1,25 @@
-import { Button } from '@heroui/react';
-import React from 'react';
-import Marquee from 'react-fast-marquee';
+import { Button } from "@heroui/react";
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const LatestTiles = ({ tiles }) => {
-    return (
-        <div className='flex justify-between bg-gray-200 items-center py-2 px-2 container mx-auto gap-2 mt-4'>
-            
-            <Button variant='outline' className="border-2 border-[#D4AF37]">Latest Tiles</Button>
+  return (
+    <div className="flex justify-between bg-gray-200 items-center py-2 px-2 container mx-auto gap-2 mt-4">
+      <Button variant="outline" className="border-2 border-[#D4AF37]">
+        Latest Tiles
+      </Button>
 
-            <Marquee pauseOnHover={true} speed={100}>
-                <span className='mx-8 font-medium'>
-                    New Arrivals:
-                </span>
+      <Marquee pauseOnHover={true} speed={100}>
+        <span className="mx-8 font-medium">New Arrivals:</span>
 
-                {tiles.map((t) => (
-                    <span className='mx-8' key={t.id}>
-                        {t.title}
-                    </span>
-                ))}
-
-            </Marquee>
-
-        </div>
-    );
+        {tiles.map((t) => (
+          <span className="mx-8" key={t.id}>
+            {t.title}
+          </span>
+        ))}
+      </Marquee>
+    </div>
+  );
 };
 
 export default LatestTiles;

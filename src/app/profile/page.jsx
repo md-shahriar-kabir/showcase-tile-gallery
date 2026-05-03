9d91d437fb8f3b53e625e831fd4a043575d9d5a9
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-03 08:31:32 +0600

    Update User Profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+'use client'
+import { UpdateUserModal } from "@/component/UpdateUserModal";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
 
 const ProfilePage = () => {
-    return (
-        <div>
-            Profile Page
-        </div>
-    );
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+  return (
+    <div>
+      <Card className="max-w-96 mx-auto flex-col items-center border mt-5">
+        <Avatar className="h-20 w-20">
+          <Avatar.Image
+            alt="John Doe"
+            src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+        </Avatar>
+        <h2 className="text-xl font-bold">{user?.name}</h2>
+        <p className="text-muted">{user?.email}</p>
+        
+        <UpdateUserModal/>
+      </Card>
+    </div>
+  );
 };
 
 export default ProfilePage;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,55 +1,86 @@
 "use client";
-import { Button } from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
-
 const Navbar = () => {
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
 
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  }
   return (
     <div className="border-b px-3">
       <nav className=" flex justify-between items-center max-w-7xl mx-auto w-full">
-        <Link href={'/'}>
-         <div className="flex gap-1 items-center">
-          <Image
-            src={"/logo.png"}
-            alt="logo"
-            loading="eager"
-            width={60}
-            height={60}
-            className="object-cover h-auto w-auto"
-          />
-          <h3 className="font-black text-[#1C1C1C] text-xl">TILE<span className="text-[#D4AF37]"> LUX</span></h3>
-        </div>
+        <Link href={"/"}>
+          <div className="flex gap-1 items-center">
+            <Image
+              src={"/logo.png"}
+              alt="logo"
+              loading="eager"
+              width={60}
+              height={60}
+              className="object-cover h-auto w-auto"
+            />
+            <h3 className="font-black text-[#1C1C1C] text-xl">
+              TILE<span className="text-[#D4AF37]"> LUX</span>
+            </h3>
+          </div>
         </Link>
 
         <ul className="flex items-center gap-5 text-sm">
           <li>
-            <Link href={"/"} className="font-semibold"><Button variant="outline">Home</Button></Link>
+            <Link href={"/"} className="font-semibold">
+              <Button variant="outline">Home</Button>
+            </Link>
           </li>
           <li>
-            <Link href={"/all-tiles"}className="font-semibold"><Button variant="outline">All Tiles</Button></Link>
+            <Link href={"/all-tiles"} className="font-semibold">
+              <Button variant="outline">All Tiles</Button>
+            </Link>
           </li>
-          
+
           <li>
-            <Link href={"/profile"}className="font-semibold"><Button variant="outline">My Profile</Button></Link>
+            <Link href={"/profile"} className="font-semibold">
+              <Button variant="outline">My Profile</Button>
+            </Link>
           </li>
         </ul>
 
-
         <div className="flex gap-4">
-          <ul className="flex items-center gap-2  text-sm">
-            <li>
-              <Link 
-              href={"/signup"}
-              ><Button variant="outline"  className="font-semibold ">Sign Up</Button> </Link>
-            </li>
-            <li>
-              <Link 
-              href={"/signin"}
-              ><Button variant="outline"  className="font-semibold ">Sign In</Button> </Link>
-            </li>
-          </ul>
+          {!user && (
+            <ul className="flex items-center gap-2  text-sm">
+              <li>
+                <Link href={"/signup"}>
+                  <Button variant="outline" className="font-semibold ">
+                    Sign Up
+                  </Button>{" "}
+                </Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>
+                  <Button variant="outline" className="font-semibold ">
+                    Sign In
+                  </Button>{" "}
+                </Link>
+              </li>
+            </ul>
+          )}
+          {user && (
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy = "no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+              <Button onClick={handleSignOut} size="sm" variant="danger">Sign Out</Button>
+            </div>
+          )}
         </div>
       </nav>
     </div>

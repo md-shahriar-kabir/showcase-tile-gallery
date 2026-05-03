ad2fcb38740f8c8c6a76e9e0f65727804d5c11ce
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-03 12:08:46 +0600

    updated link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://showcase-tile-gallery.vercel.app/"
+    baseURL: "https://showcase-tile-gallery.vercel.app"
 })
